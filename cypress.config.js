7c15ca33e5fe6e00becbf97f3510b00c17153dc1
murilogalindo <murilomag.galindo@gmail.com>
2025-04-26 22:44:42 -0300

    configurando o mochawesome no cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,23 @@
-//Arquivo de configuração/entry point geral do Cypress
+// Arquivo de configuração/entry point geral do Cypress
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   chromeWebSecurity: false,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',  // Define o padrão para localizar os testes
+    supportFile: false,  // Desabilita o arquivo de suporte (se não necessário)
+    video: false,  // Desabilita a gravação de vídeos dos testes (opcional)
+    reporter: 'mochawesome',  // Define o reporter para gerar os relatórios com Mochawesome
+    reporterOptions: {
+      reportDir: 'cypress/reports',  // Diretório onde os relatórios serão armazenados
+      overwrite: false,  // Não sobrescrever relatórios antigos
+      html: true,  // Gera o relatório em formato HTML
+      json: true  // Gera o relatório em formato JSON
     },
-    baseUrl: 'https://front.serverest.dev/login',
-    //não vai limpar o estado da tela após cada it
-    testIsolation: false
+    baseUrl: 'https://front.serverest.dev/login',  // Define a URL base para os testes
+    testIsolation: false,  // Não vai limpar o estado da tela após cada it
+    setupNodeEvents(on, config) {
+      // Implementação de listeners de eventos, se necessário
+    }
   }
 });
--- cypress/e2e/productRegistration.spec.cy.js
+++ cypress/e2e/productRegistration.spec.cy.js
@@ -1,31 +1,34 @@
 import productRegistration from '../pages/productRegistrationPage'
 import userLogin from '../pages/userLoginPage'
 
+const nomeAleatorio = 'Usuário Teste' + Math.random().toString(36).substring(7);
+const numero =  Math.random().toString(36).substring(7);
 
 describe('Registration new product', () => {
     it('Must be possible create a new product no aplication', () => {
 
+        
         productRegistration.visit();
         userLogin.acessTheLoginPage({
             email: 'murilomag.galindo@gmail.com',
             senha: '123456'
             
         })
+        
         cy.get('[data-testid="cadastrarProdutos"]').click();
 
         productRegistration.fillProductRegistration({
-            nome: 'Laptop Surface',
-            preco: '25',
+            nome: nomeAleatorio,
+            preco: numero,
             descricao: 'Microsoft Novo laptop Surface (7ª edição) - Windows 11 Home Copilot + PC - Tela LCD PixelSense de 13,8 polegadas - Qualcomm Snapdragon X Elite (12 núcleos) - RAM de 32 GB - SSD de 1 TB - Grafite - ',
-            quantidade: '2500',
+            quantidade: numero,
             imagem: 'cypress/fixtures/produto-laptop.png'
 
         })
+        cy.contains('Listar Produtos').should('be.visible');
         cy.location('href').should('eq', 'https://front.serverest.dev/admin/listarprodutos');
+        
 
-
-
-
-    })
+    });
 
 })
